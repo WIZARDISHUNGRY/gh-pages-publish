da2195a8bc47682ec8a7c7a5e256ce00ba815ba4
Jon Williams <jon.williams@hollywood.com>
2012-09-10 17:44:18 -0400

    go fmt

--- gh-pages.go
+++ gh-pages.go
@@ -2,9 +2,9 @@ package githubPagesPublish
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
-	"io/ioutil"
 	"regexp"
 	"strings"
 )
@@ -16,20 +16,20 @@ type Publisher struct {
 }
 
 func (p Publisher) gitRun(args []string) error {
-  cmd := exec.Command("git",args...)
-  cmd.Stdout = os.Stdout
-  cmd.Stdout = os.Stdout
-  cmd.Dir = p.Path
-  fmt.Fprintf(os.Stderr, "Running `git %s`\n",strings.Join(args, " "))
-  err := cmd.Start()
-  if err != nil {
-	  fmt.Fprintf(os.Stderr, "err %v\n", err)
-	  return err
-  }
-  fmt.Fprintf(os.Stderr, "Waiting for command to finish...\n")
-  err = cmd.Wait()
-  fmt.Fprintf(os.Stderr, "Command finished with error: %v\n", err)
-  return err
+	cmd := exec.Command("git", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stdout = os.Stdout
+	cmd.Dir = p.Path
+	fmt.Fprintf(os.Stderr, "Running `git %s`\n", strings.Join(args, " "))
+	err := cmd.Start()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err %v\n", err)
+		return err
+	}
+	fmt.Fprintf(os.Stderr, "Waiting for command to finish...\n")
+	err = cmd.Wait()
+	fmt.Fprintf(os.Stderr, "Command finished with error: %v\n", err)
+	return err
 }
 
 func New(uri, branch string) (*Publisher, error) {
@@ -41,38 +41,38 @@ func New(uri, branch string) (*Publisher, error) {
 	p.Path, _ = p.pathName()
 
 	args := []string{
-	  "clone",
-	  "--depth", "1",
-	  "-b", p.Branch,
-	  p.Uri,
-	  p.Path,
+		"clone",
+		"--depth", "1",
+		"-b", p.Branch,
+		p.Uri,
+		p.Path,
 	}
 	fmt.Fprintf(os.Stderr, "git clone: %s\n", strings.Join(args, " "))
 	err := p.gitRun(args)
 	if err != nil {
-	  return nil, err
+		return nil, err
 	}
 
 	args = []string{
-	  "checkout",
-	  p.Branch,
+		"checkout",
+		p.Branch,
 	}
 	err = p.gitRun(args)
 	if err != nil {
-	  // Create it!
-	  	args = []string{
-		  "checkout",
-		  "--orphan",
-		  p.Branch,
+		// Create it!
+		args = []string{
+			"checkout",
+			"--orphan",
+			p.Branch,
 		}
 		err = p.gitRun(args)
 		if err != nil {
-		  return nil, err
+			return nil, err
 		}
 	}
 
 	args = []string{
-	  "branch",
+		"branch",
 	}
 	err = p.gitRun(args)
 	return p, nil
@@ -87,39 +87,39 @@ func (p Publisher) pathName() (name string, err error) {
 		fmt.Sprintf("%d", os.Getpid()),
 		"",
 	}
-	return ioutil.TempDir("",strings.Join(s, "__")) 
+	return ioutil.TempDir("", strings.Join(s, "__"))
 }
 
 func (p Publisher) Push() error {
-  args := []string{
-	"add",
-	".",
-  }
-  err := p.gitRun(args)
-  if err != nil {
-	return err
-  }
+	args := []string{
+		"add",
+		".",
+	}
+	err := p.gitRun(args)
+	if err != nil {
+		return err
+	}
 
-  args = []string{
-	"commit",
-	"-a",
-	"-m",
-	"Automatic commit from Go!",
-  }
-  err = p.gitRun(args)
-  if err != nil {
-	return err
-  }
+	args = []string{
+		"commit",
+		"-a",
+		"-m",
+		"Automatic commit from Go!",
+	}
+	err = p.gitRun(args)
+	if err != nil {
+		return err
+	}
 
-  args = []string{
-	"push",
-	"origin",
-	p.Branch,
-  }
-  return p.gitRun(args)
+	args = []string{
+		"push",
+		"origin",
+		p.Branch,
+	}
+	return p.gitRun(args)
 }
 
 func (p Publisher) Close() error {
-  fmt.Fprintf(os.Stderr, "Removing path %s\n", p.Path)
-  return os.RemoveAll(p.Path)
+	fmt.Fprintf(os.Stderr, "Removing path %s\n", p.Path)
+	return os.RemoveAll(p.Path)
 }
